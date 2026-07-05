bf5e044df26d917fa5a60b81988ba943436c1888
hz3084-ui <hz3084@columbia.edu>
2026-07-06 00:04:53 +0800

    Complete getBondDuration

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,8 +1,23 @@
 import numpy as np
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    if ppy == 1:
-        x = 8.51
-    if ppy == 2:
-        x = 8.42
-    return(x)
+def getBondDuration(y, face, couponRate, m, ppy=1):
+
+    coupon = face * couponRate / ppy
+    bondPrice = 0
+    weightedPV = 0
+
+    for t in range(1, m * ppy + 1):
+
+        cf = coupon
+
+        if t == m * ppy:
+            cf += face
+
+        pv = cf / (1 + y / ppy) ** t
+
+        bondPrice += pv
+        weightedPV += (t / ppy) * pv
+
+    bondDuration = weightedPV / bondPrice
+
+    return bondDuration
